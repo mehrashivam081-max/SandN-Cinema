084701f25b8fe1805a346add90166e7222096d2a
mehrashivam081-max <mehrashivam081@gmail.com>
2026-04-06 23:50:18 +0530

    Fixed assets 404 path issue in vite.config

--- sandn-cinema-frontend/vite.config.js
+++ sandn-cinema-frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // 👈 YEH EKDUM AISA HONA CHAHIYE (Purana SandN-Cinema hata do)
   plugins: [react()],
-  base: '/SandN-Cinema/', // ✅ Ye line jodni hai
 })
